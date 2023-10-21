2531747c3ace74fb225d73162ab980a54e96fb04
IsakLarsson <isaklarsson98@hotmail.com>
2023-10-21 18:28:01 +0200

    remove logs

--- counter.js
+++ counter.js
@@ -3,14 +3,12 @@ let holdStart = 0;
 
 const setCounter = (element) => {
   element.innerHTML = `${holdTime}ms`;
-  console.log(holdTime);
 };
 
 export function setupCounter(element) {
   element.addEventListener("mousedown", () => (holdStart = Date.now()));
   element.addEventListener("mouseup", () => {
     holdTime = Math.abs(holdStart - Date.now());
-    console.log(holdTime);
     setCounter(document.querySelector("#milliseconds"));
   });
 }
