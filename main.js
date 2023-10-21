1d822f148eb8ea9ced9f26646ec9b51d2c101117
IsakLarsson <isaklarsson98@hotmail.com>
2023-10-21 19:31:04 +0200

    allows any key to be clicked instead of a button

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 import "./style.css";
-import { setupCounter } from "./counter.js";
+
+let keyPressStartTime = 0;
+let keyPressEndTime = 0;
+let keyPressDuration = 0;
+
+document.addEventListener("keydown", () => {
+  if (keyPressStartTime === 0) {
+    keyPressStartTime = Date.now();
+  }
+});
+
+document.addEventListener("keyup", () => {
+  keyPressEndTime = Date.now();
+  keyPressDuration = keyPressEndTime - keyPressStartTime;
+  document.getElementById("milliseconds").innerHTML = `${keyPressDuration}ms`;
+  keyPressStartTime = 0;
+});
 
 document.querySelector("#app").innerHTML = `
   <div>
+    <div>
+      <h3>Hold any key!</h1>
+    </div>
     <div class="result">
       <h1 id="milliseconds">...</h2>
     </div>
-    <div class="button">
-      <button id="counter" type="button">Hold me</button>
-    </div>
   </div>
 `;
-setupCounter(document.querySelector("#counter"));
+
+// setupCounter(document.querySelector("#counter"));
